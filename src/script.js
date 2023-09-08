43a95e4bb1db66d8cdb117105c2b1d6691841d0a
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-09-09 00:36:47 +0100

    Actual time set

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+        if (hours < 10){
+            hours = `0${hours}`;
+        }
+    let minutes = date.getMinutes();
+        if (minutes < 10){
+            minutes = `0${minutes}`;
+        }
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ];
+    return `${day} ${hours} : ${minutes}`;
+}
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -5,12 +28,14 @@ function displayTemperature(response){
     let temperatureNightElement = document.querySelector("#temperature-night");
     let humidityConditionElement = document.querySelector("#humidity");
     let windConditionElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     temperatureDayElement.innerHTML = Math.round(response.data.main.temp_max);
     temperatureNightElement.innerHTML = Math.round(response.data.main.temp_min);
     humidityConditionElement.innerHTML = response.data.main.humidity;
     windConditionElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     console.log(response);
 }
 
